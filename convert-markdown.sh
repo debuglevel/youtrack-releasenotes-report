c45b3a577db82e4682bb7268e6b4f2aae77d7a1d
Marc Kohaupt <marc.kohaupt@avm-d.de>
2021-06-11 17:00:39 +0200

    German language settings for pandoc/latex

--- convert-markdown.sh
+++ convert-markdown.sh
@@ -9,7 +9,7 @@ for filetype in "${filetypes[@]}"
 do
   echo "=== Converting to $filetype..."
   # Use pandoc 2.0 or newer! pandoc < 2.0 will not convert list indentations correctly.
-  pandoc --metadata title="$TITLE" --toc -s --css pandoc.css intermediate.md -o "${OUTPUT_BASENAME}$filetype"
+  pandoc -V lang=de --metadata title="$TITLE" --toc -s --css pandoc.css intermediate.md -o "${OUTPUT_BASENAME}$filetype"
   echo "=== Converted to $filetype"
 done
 
