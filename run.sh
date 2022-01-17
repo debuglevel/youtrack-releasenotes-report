376818671bfa8ec6fe3d8a2a1f76f7d5a3607b0c
Marc Kohaupt <marc.kohaupt@avm-d.de>
2022-01-17 12:09:46 +0100

    Fix run.sh to pass environment vars

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir -p out
 rm -r out/* &> /dev/null
 
 echo "= Fetching release notes and generating Markdown..."
-python3 main.py --field-name="$FIELD_NAME" --field-value="$FIELD_VALUE" --output-basename="$OUTPUT_BASENAME" "$HOST" "$USER" "$PASSWORD"
+python3 main.py --field-name="$FIELD_NAME" --field-value="$FIELD_VALUE" --output-basename="$OUTPUT_BASENAME" "$YOUTRACK_URL" "$HUB_URL" "$TOKEN"
 
 echo "= Converting to additional formats using pandoc..."
 cp pandoc.css out/
